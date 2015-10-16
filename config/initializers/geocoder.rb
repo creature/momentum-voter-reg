be7e6d036443c4f2e1f39b78d22afd06bc99e497
James Darling <james@abscond.org>
2015-10-16 03:16:12 +0100

    Disable SSL certificate verification in development mode.
    This is always a pain when I’m in a hurry

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,6 @@
 if ENV['GEOCODER_API_KEY'].present?
   Geocoder.configure(api_key: ENV['GEOCODER_API_KEY'])
 end
+if Rails.env == "development"
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
